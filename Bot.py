cb3479dd6ca0ab735a128d98149f8fe785f07075
exCR0SS <119153683+exCR0SS@users.noreply.github.com>
2023-03-08 23:07:49 +0500

    Переписал вызов парсера, иначе выдавал на актуальную инфу

--- Bot.py
+++ Bot.py
@@ -3,12 +3,12 @@ import Parser
 from settings import api_key
 
 bot = telebot.TeleBot(api_key)
-bosses = Parser.get_bosses()
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['boss'])
 def start_bot(message):
-    bot.send_message(message.chat.id, f'Список эпик боссов в порядке от ближайшего до удалённого:')
+    bosses = Parser.get_bosses()
+    bot.send_message(message.chat.id, f'Список эпик боссов в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
 
