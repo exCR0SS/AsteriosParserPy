1eb133f3dc28864dffd3e1d917ca2dcb488136f9
exCR0SS <119153683+exCR0SS@users.noreply.github.com>
2023-03-10 09:19:54 +0500

    Переписал логику, добавил получение списка боссов с других серверов проекта

--- Bot.py
+++ Bot.py
@@ -1,19 +1,77 @@
 import telebot
 import Parser
 from settings import api_key
+from settings import linkx55
+from settings import linkx5
+from settings import linkx7
+from settings import linkx3
+from settings import linkx1
+from settings import bosses_respawn_days_setting
 
 # ключ бота в файле settings, в github не отправляется
 bot = telebot.TeleBot(api_key)
 
 
-# по команде боту /boss вызывает парсер, получает словарь с именами и датами респа боссов,
-# через цикл отдельными сообщениями отвечает пользователю в чат
-@bot.message_handler(commands=['boss'])
+@bot.message_handler(commands=['start'])
 def start_bot(message):
-    bosses = Parser.get_bosses()
-    bot.send_message(message.chat.id, f'Список эпик боссов в порядке от ближайшего:')
+    bot.send_message(message.chat.id, f'Доступные команды:\n'
+                                      f'Эпики серверов:\n'
+                                      f'x55 - /bossX55\n'
+                                      f'x5 - /bossX5\n'
+                                      f'x7 - /bossX7\n'
+                                      f'x3 - /bossX3\n'
+                                      f'x1 - /bossX1')
+
+
+@bot.message_handler(commands=['bossX55'])
+def get_bosses_x55(message):
+    bosses = Parser.get_bosses(linkx55)
+    bot.send_message(message.chat.id, f'Список эпик боссов сервера х55 в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
+
+
+@bot.message_handler(commands=['bossX5'])
+def get_bosses_x5(message):
+    bosses = Parser.get_bosses(linkx5)
+    bot.send_message(message.chat.id, f'Список эпик боссов сервера х5 в порядке от ближайшего:')
+    for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
+        bot.send_message(message.chat.id, f'{key}: {value}')
+
+
+@bot.message_handler(commands=['bossX7'])
+def get_bosses_x7(message):
+    bosses = Parser.get_bosses(linkx7)
+    bot.send_message(message.chat.id, f'Список эпик боссов сервера х7 в порядке от ближайшего:')
+    for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
+        bot.send_message(message.chat.id, f'{key}: {value}')
+
+
+@bot.message_handler(commands=['bossX1'])
+def get_bosses_x1(message):
+    bosses = Parser.get_bosses(linkx1)
+    bot.send_message(message.chat.id, f'Список эпик боссов сервера х1 в порядке от ближайшего:')
+    for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
+        bot.send_message(message.chat.id, f'{key}: {value}')
+
+
+@bot.message_handler(commands=['bossX3'])
+def get_bosses_x3(message):
+    bosses = Parser.get_bosses(linkx3)
+    bot.send_message(message.chat.id, f'Список эпик боссов сервера х3 в порядке от ближайшего:')
+    for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
+        bot.send_message(message.chat.id, f'{key}: {value}')
+
+
+@bot.message_handler(commands=['author'])
+def get_author(message):
+    bot.send_message(message.chat.id, f'Автор: iCross')
+
+
+@bot.message_handler(commands=['settings'])
+def get_author(message):
+    bot.send_message(message.chat.id,
+                     f'Текущие настройки времени возрождения боссов (в днях):{bosses_respawn_days_setting}')
 
 
 bot.polling()
--- Parser.py
+++ Parser.py
@@ -3,43 +3,47 @@ from selenium.webdriver.common.by import By
 import datetime
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from settings import bosses_respawn_days_setting
 
 
-def get_bosses():
-    res_check = []
-    death_dict = {}
-    new_format = "%d.%m.%Y"
-    old_format = '%Y-%m-%d'
-    bosses_dict = {'Baium': 4, 'Antharas': 8, 'Valakas': 8, 'Orfen': 3, 'Queen': 2, 'Core': 1, 'Ant': 2}
+def get_bosses(link_serv):
+    __link = link_serv
+    __res_check = []
+    __death_dict = {}
+    __new_format = "%d.%m.%Y"
+    __old_format = '%Y-%m-%d'
+    # bosses_dict = {'Baium': 4, 'Antharas': 8, 'Valakas': 8, 'Orfen': 3, 'Queen': 2, 'Core': 1, 'Ant': 2}
+    __bosses_respawn_days = bosses_respawn_days_setting
 
     # Настройка аргументов и опций
-    options = webdriver.ChromeOptions()
-    options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 FireFox/84.0")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--no-sandbox")
-    options.headless = True
+    __options = webdriver.ChromeOptions()
+    __options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 FireFox/84.0")
+    __options.add_argument("--disable-blink-features=AutomationControlled")
+    __options.add_argument("--no-sandbox")
+    __options.headless = True
 
     # Парсинг
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    browser.get('https://ru.asterios.tm/index.php?cmd=rss&serv=2&filter=epic')
-    block = browser.find_element(By.CLASS_NAME, 'center')
-    all_bosses = block.find_elements(By.TAG_NAME, 'a')
+    __browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=__options)
+    __browser.get(__link)
+    __block = __browser.find_element(By.CLASS_NAME, 'center')
+    __all_bosses = __block.find_elements(By.TAG_NAME, 'a')
 
     # Все записи записываем в список
-    for boss in all_bosses:
-        res_check.append(boss.text)
+    for boss in __all_bosses:
+        __res_check.append(boss.text)
 
     # На основе списка формируем словарь из даты и имени босса
-    for i in list(reversed(res_check)):
+    for i in list(reversed(__res_check)):
         x, y, z, u, *o = i.split(" ")
         for p in o:
-            # death_dict[p] = [x]
-            death_dict[p] = (
-                    datetime.datetime.strptime(x, old_format) + datetime.timedelta(days=bosses_dict[p])).strftime(
-                new_format)
+            # __death_dict[p] = [x]
+            __death_dict[p] = (
+                    datetime.datetime.strptime(x, __old_format) + datetime.timedelta(
+                days=__bosses_respawn_days[p])).strftime(__new_format)
             continue
     # Удаление лишнего босса Core. Queen Ant добавляется как 2 босса, Ant удаляем, а ключ Queen заменяем на Queen Ant
-    del death_dict['Ant']
-    del death_dict['Core']
-    death_dict['Queen Ant'] = death_dict.pop('Queen')
-    return death_dict
+    if 'Core' in __death_dict:
+        del __death_dict['Core']
+    del __death_dict['Ant']
+    __death_dict['Queen Ant'] = __death_dict.pop('Queen')
+    return __death_dict
