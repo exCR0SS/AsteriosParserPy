fa8b5b8d1be98312406a7e9a4f61e499ae3af42c
exCR0SS <119153683+exCR0SS@users.noreply.github.com>
2023-03-18 16:01:48 +0500

    Новый функционал, список телепортов по замкам / фортам

--- Bot.py
+++ Bot.py
@@ -1,16 +1,23 @@
 import telebot
 import Parser
-from settings import api_key
-from settings import linkx55
-from settings import linkx5
-from settings import linkx7
-from settings import linkx3
-from settings import linkx1
-from settings import bosses_respawn
+import settings
 
 # ключ бота в файле settings, в github не отправляется
-bot = telebot.TeleBot(api_key)
-last_time = {}
+bot = telebot.TeleBot(settings.api_key)
+
+
+# pm2 start Bot.py --interpreter=python3 для запуска на сервере
+
+# last_message = {}
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     if message.from_user.id not in last_message:
+#         last_message[message.from_user.id] = message.date - 5
+#         print(last_message)
+#     if message.date - last_message[message.from_user.id] > 5:
+#         bot.reply_to(message, message.text)
+#         last_message[message.from_user.id] = message.date
+#         print(last_message)
 
 
 @bot.message_handler(commands=['start'])
@@ -26,7 +33,7 @@ def start_bot(message):
 
 @bot.message_handler(commands=['bossX55'])
 def get_bosses_x55(message):
-    bosses = Parser.get_bosses(linkx55)
+    bosses = Parser.get_bosses(settings.linkx55)
     bot.send_message(message.chat.id, f'Список эпик боссов сервера х55 в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
@@ -34,7 +41,7 @@ def get_bosses_x55(message):
 
 @bot.message_handler(commands=['bossX5'])
 def get_bosses_x5(message):
-    bosses = Parser.get_bosses(linkx5)
+    bosses = Parser.get_bosses(settings.linkx5)
     bot.send_message(message.chat.id, f'Список эпик боссов сервера х5 в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
@@ -42,7 +49,7 @@ def get_bosses_x5(message):
 
 @bot.message_handler(commands=['bossX7'])
 def get_bosses_x7(message):
-    bosses = Parser.get_bosses(linkx7)
+    bosses = Parser.get_bosses(settings.linkx7)
     bot.send_message(message.chat.id, f'Список эпик боссов сервера х7 в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
@@ -50,7 +57,7 @@ def get_bosses_x7(message):
 
 @bot.message_handler(commands=['bossX1'])
 def get_bosses_x1(message):
-    bosses = Parser.get_bosses(linkx1)
+    bosses = Parser.get_bosses(settings.linkx1)
     bot.send_message(message.chat.id, f'Список эпик боссов сервера х1 в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
@@ -58,7 +65,7 @@ def get_bosses_x1(message):
 
 @bot.message_handler(commands=['bossX3'])
 def get_bosses_x3(message):
-    bosses = Parser.get_bosses(linkx3)
+    bosses = Parser.get_bosses(settings.linkx3)
     bot.send_message(message.chat.id, f'Список эпик боссов сервера х3 в порядке от ближайшего:')
     for key, value in sorted(bosses.items(), key=lambda date: (date[1], date[0])):
         bot.send_message(message.chat.id, f'{key}: {value}')
@@ -72,7 +79,48 @@ def get_author(message):
 @bot.message_handler(commands=['settings'])
 def get_author(message):
     bot.send_message(message.chat.id,
-                     f'Текущие настройки времени возрождения боссов (в днях):{bosses_respawn}')
+                     f'Текущие настройки времени возрождения боссов (в днях):{settings.bosses_respawn}')
+
+
+@bot.message_handler(commands=['fortress'])
+def get_author(message):
+    fort = bot.send_message(message.chat.id, 'Название форта?')
+    result = bot.register_next_step_handler(fort, get_castles_fortress_tp(message.chat.id, False))
+    # bot.reply_to(message.chat.id, )
+    print(result)
+    # bot.send_message(message.chat.id,
+    #                  f'Телепорты к фортам через Alt + B:\n {settings.fortress_tp}')
+
+
+@bot.message_handler(commands=['castles'])
+def get_author(message):
+    fort = bot.send_message(message.chat.id, 'Название замка?')
+    bot.register_next_step_handler(fort, get_castles_fortress_tp(message.chat.id, True))
+
+
+# @bot.message_handler(commands=['castle'])
+# def get_siege_clanes(message):
+#     castles = Parser.get_castles_siege()
+#     bot.send_message(message.chat.id, f'Нажимаю на кнопку')
+
+# @bot.message_handler(func=lambda message: message.reply_to_message != None)
+# def reply_message_handler(message):
+#     bot.send_message(chat_id=message.reply_to_message.from_user.id, text=message.text)
+
+
+@bot.message_handler(content_types=['text'])
+def get_castles_fortress_tp(message, castle):
+    user_request = message
+    if castle is True:
+        if user_request in settings.castles_tp:
+            return bot.reply_to(message, settings.castles_tp.values())
+        # else:
+        #     return bot.reply_to(message, 'Такого замка нет')
+    else:
+        if user_request in settings.fortress_tp:
+            return bot.reply_to(message, settings.fortress_tp.values())
+        # else:
+        #     return bot.reply_to(message, 'Такого форта нет')
 
 
 bot.polling()
--- Parser.py
+++ Parser.py
@@ -4,6 +4,9 @@ import datetime
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from settings import bosses_respawn_days_setting
+from settings import link_events
+from selenium.webdriver.support.ui import Select
+import time
 
 
 def get_bosses(link_serv):
@@ -28,7 +31,7 @@ def get_bosses(link_serv):
     __block = __browser.find_element(By.CLASS_NAME, 'center')
     __all_bosses = __block.find_elements(By.TAG_NAME, 'a')
 
-    # Все записи записываем в список
+    # Из записей формируем список
     for boss in __all_bosses:
         __res_check.append(boss.text)
 
@@ -41,9 +44,40 @@ def get_bosses(link_serv):
                     datetime.datetime.strptime(x, __old_format) + datetime.timedelta(
                 days=__bosses_respawn_days[p])).strftime(__new_format)
             continue
-    # Удаление лишнего босса Core. Queen Ant добавляется как 2 босса, Ant удаляем, а ключ Queen заменяем на Queen Ant
+    # Удаление лишнего босса Core. Queen Ant добавляется как 2 босса, Ant удаляем, а ключ Queen заменяем на Queen Ant.
+    # переписаны на if чтобы избежать KeyError, когда боссы ещё не убиты (актуально для переоткрытия серверов)
     if 'Core' in __death_dict:
         del __death_dict['Core']
-    del __death_dict['Ant']
-    __death_dict['Queen Ant'] = __death_dict.pop('Queen')
+    if 'Ant' in __death_dict:
+        del __death_dict['Ant']
+    if 'Queen' in __death_dict:
+        __death_dict['Queen Ant'] = __death_dict.pop('Queen')
     return __death_dict
+
+
+# def get_castles_siege():
+#     __link = link_events
+#     __servers_dict = {}
+#     __castles_dict = {}
+#     # __serv = serv
+#
+#     # Настройка аргументов и опций
+#     __options = webdriver.ChromeOptions()
+#     __options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 FireFox/84.0")
+#     __options.add_argument("--disable-blink-features=AutomationControlled")
+#     __options.add_argument("--no-sandbox")
+#
+#     # Парсинг
+#     __browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=__options)
+#     __browser.get(__link)
+#     __block = __browser.find_element(By.CLASS_NAME, 'tx3')
+#     # __all_castles = __block.find_elements(By.TAG_NAME, 'a')
+#     __button_castles = __block.find_element(By.XPATH, "// *[ @ id = 'page_contents'] / div[1] / a[5]").click()
+#
+#     __block_serv_castles = __browser.find_elements(By.CLASS_NAME, 'vubor')
+#     __change_serv = __block_serv_castles(by=By.CLASS_NAME, value='option')
+#     # __change_serv = __block.find_elements(By.CLASS_NAME, 'vubor')
+#     __select_serv = Select(__change_serv)
+#     __select_serv.select_by_value('2')
+#     __button_serv = __block_serv_castles.find_element(By.XPATH, '/html/body/div/div[3]/div/div/table/tbody/tr/td[2]/div[1]/div[3]/form/p/a').click()
+#     time.sleep(10)
